6a2aa203825019d9e0b67b64d26310f20530c2f8
arif20026 <arifislamipe026@gmail.com>
2023-11-28 23:12:47 +0600

    added cart section

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,73 @@ import Cards from "./Components/Cards/Cards";
 import Cart from "./Components/Cart/Cart";
 import { useState } from "react";
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+
 const App = () => {
 
-  const [cards, setCards] =useState([])
-  const [credit, setCredit] =useState(0)
+  const [cards, setCards] = useState([])
+  const [credit, setCredit] = useState(0)
 
-  const handleAddToCart= card => {
-    const newCards =[...cards,card]
-    setCards(newCards)
+  const handleAddToCart = card => {
+    if (!cards.includes(card)) {
+
+      // console.log(card.id)
+
+      const newCards = [...cards, card]
+      setCards(newCards)
+
+    }
+
+    
+    else {
+      setCards(cards)
+      toast.error('Course already added!', {
+        position: 'top-right',
+        autoClose: 2000, 
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        
+      });
+
+    }
   }
 
-  const handleAddCredit = nwCredit =>{
+    
+  const handleAddCredit = nwCredit => {
+    
     const newCredit = credit + nwCredit
-    setCredit(newCredit)
+
+    if(newCredit<=20){
+
+      setCredit(newCredit)
+
+    }
+    else{
+      setCredit(20)
+      toast.error('Can not add more than 20 credit!', {
+        position: 'top-right',
+        autoClose: 2000, 
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        
+      });
+
+    }
+    
+   
   }
 
-  
+
+ 
+
+ 
+
+
   return (
 
     <>
@@ -28,15 +79,17 @@ const App = () => {
 
       <div className="md:flex mx-16">
 
-      <Cards 
-      handleAddToCart={handleAddToCart}
-       handleAddCredit={handleAddCredit}>
+        <Cards
+          handleAddToCart={handleAddToCart}
+          handleAddCredit={handleAddCredit}>
 
-       </Cards>
-      <Cart cards={cards} credit={credit}></Cart>
+        </Cards>
+        <Cart cards={cards} credit={credit}></Cart>
+
+        <ToastContainer />
 
       </div>
-      
+
 
 
     </>
